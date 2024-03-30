e12a9564bd657cefe60ab73cd9f56a342fc5be4f
Siarhei Zavadski <siarhei.zavadski@gmail.com>
2024-03-30 21:27:57 +0100

    gh pages fix

--- src/components/inputForm/inputForm.jsx
+++ src/components/inputForm/inputForm.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from 'react';
+import { React, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { tr as intl } from '../local';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
