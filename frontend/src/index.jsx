c96b03fc974e0573a04f6fb5c988d82dc5b9858b
Siarhei Zavadski <siarhei.zavadski@gmail.com>
2025-03-18 20:09:48 +0100

    fixing styles after deployment

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import ThemeProvider from './provider/themeProvider';
 import { RecoilRoot } from 'recoil';
-import './index.module.scss';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
